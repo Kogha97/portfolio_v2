52c36948f2fa7c1d1e5cfa595178a438f7b49f7c
Kogha97 <poiuytrewq7991@gmail.com>
2024-05-15 14:02:11 +0100

    typing animation added

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 export default function Introduction() {
+  const el = useRef(null);
+  const typed = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        'João Augusto.'
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: '|',
+      loop: false
+    };
+    
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+  
+      typed.current.destroy();
+    }
+  }, []);
+
   return (
     <div className='introduction'>
-            <h1>Hi, my name is</h1>
-            <h2>João Augusto.</h2>
-            <h3>I craft digital soluctions for the online realm.</h3>
-           <p>I am a new and excited Full Stack Web & App Developer, looking to emerge myself into the world of technology, ready to adapt, learn and create a digital Paragon. I can not wait for this new adventure!</p>
+      <h1>Hi, my name is</h1>
+      <h2><span ref={el}></span></h2>
+      <h3>I craft digital solutions for the online realm.</h3>
+      <p>I am a new and excited Full Stack Web & App Developer, looking to emerge myself into the world of technology, ready to adapt, learn, and create a digital Paragon. I cannot wait for this new adventure!</p>
     </div>
-  )
+  );
 }
