eebe9a572aaf9c991f8824ed0b3aa910659f065e
Kogha97 <poiuytrewq7991@gmail.com>
2024-05-20 10:38:24 +0100

    animation added to under construction, little media queries added

--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { faHammer } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function UnderConstruction() {
   const navigate = useNavigate();
 
   return (
-    <div className='UnderConstructionContainer'>
-      <h2>This project is still under construction  </h2>
-        <h2><a href="/" onClick={(e) => {e.preventDefault(); navigate('/');}} className="goBackLink">Click here</a> to go back.</h2>
+    <div className='under-construction-container'>
+      <h2>
+        This project is still under construction   <FontAwesomeIcon icon={faHammer} className="hammer-icon" />
+      </h2>
+      <h2>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate('/');
+          }}
+          className="go-back-link"
+        >
+          Click here
+        </a> to go back.
+      </h2>
     </div>
   );
 }
